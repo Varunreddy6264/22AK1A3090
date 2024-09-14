9de1745d709826e17d73a03d0fb1a12a728d5ead
Varunreddy6264 <varunreddy7456@gmail.com>
2024-09-14 14:26:37 +0530

    Add files via upload

---
+++ 00.py
@@ -0,0 +1,15 @@
+def add_numbers(a, b)
+
+
+    return a + b
+
+print("Sum is: " + add_numbers(5, 10))
+
+for i in range(5)
+    print(i)
+
+if True
+    print("This is true")
+
+x = [1, 2, 3
+print(x[3])
